873bf8b2e11f9f095c2c515f5e6e42d8f8bef13b
JonahChrzanowski <42982132+JonahChrzanowski@users.noreply.github.com>
2018-09-06 20:57:36 -0400

    Add files via upload
    Powershell script to collect and print various information about Windows systems. Written for Computer System Forensics.

---
+++ collect.ps1
@@ -0,0 +1,28 @@
+﻿Get-Date
+Get-TimeZone
+[System.Environment]::OSVersion.Version
+Get-CimInstance -ClassName Win32_ComputerSystem
+Get-ComputerInfo
+Get-ADDomain
+Get-WmiObject -Class Win32_UserAccount
+Get-Service
+Get-WmiObject -Class Win32_Product
+Get-ScheduledTask
+Get-NetNeighbor
+foreach ($i in ((Get-NetLbfoTeam).name)) 
+{ 
+Get-NetAdapter (Get-NetLbfoTeamMember -Team $i).Name | Format-Table 
+}
+Get-NetRoute
+Get-NetIPAddress
+Get-DhcpServerInDC
+Get-DnsServer
+Get-NetTCPConnection
+Get-DnsClientCache
+Get-SmbShare
+Get-Printer
+Get-ItemProperty HKLM:\Software\Wow6432Node\Microsoft\Windows\CurrentVersion\Uninstall\* | Select-Object DisplayName, DisplayVersion, Publisher, InstallDate | Format-Table –AutoSize
+Get-Process
+Get-WindowsDriver
+Get-ChildItem -Path Downloads
+Get-ChildItem -Path Documents
