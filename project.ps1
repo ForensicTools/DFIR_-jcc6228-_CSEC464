85b0851f5f0f3a41c5c366719298160844f065f6
JonahChrzanowski <42982132+JonahChrzanowski@users.noreply.github.com>
2018-11-14 15:58:45 -0500

    Update project.ps1
    First commit: Adds features to display the first 1000 entries of the Windows Application and System logs, which I have chosen due to that they are the most frequently used. Also displays stats on what types of logs are being made across the entirety of the collected data.

--- project.ps1
+++ project.ps1
@@ -1,1 +1,14 @@
 #here we will be making a script to collect logs in windows
+echo "The last 1000 entries from the system log: "
+Get-Eventlog -LogName system -Newest 1000
+echo ""
+echo "System Log Statistics:"
+$Events = Get-Eventlog -LogName system
+$Events | Group-Object -Property source -noelement | Sort-Object -Property count -Descending
+echo ""
+echo "The last 1000 entries from the Application log: "
+Get-Eventlog -LogName system -Newest 1000
+echo ""
+echo "Application Log Statistics"
+$Events = Get-Eventlog -LogName Application
+$Events | Group-Object -Property source -noelement | Sort-Object -Property count -Descending
