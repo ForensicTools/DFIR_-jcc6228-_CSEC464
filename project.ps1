3ad55c43ed4ccb0ea150d0dca66e6455560b5d71
JonahChrzanowski <42982132+JonahChrzanowski@users.noreply.github.com>
2018-11-14 22:10:04 -0500

    Update project.ps1
    More features added. Functions to view Administrator account activity and system errors between a specific time frame.

--- project.ps1
+++ project.ps1
@@ -1,5 +1,5 @@
 #here we will be making a script to collect logs in windows
-echo "The last 1000 entries from the system log: "
+echo "The last 1000 entries from the system log:"
 Get-Eventlog -LogName system -Newest 1000
 echo ""
 echo "System Log Statistics:"
@@ -9,6 +9,19 @@ echo ""
 echo "The last 1000 entries from the Application log: "
 Get-Eventlog -LogName system -Newest 1000
 echo ""
-echo "Application Log Statistics"
+echo "Application Log Statistics:"
 $Events = Get-Eventlog -LogName Application
 $Events | Group-Object -Property source -noelement | Sort-Object -Property count -Descending
+echo ""
+echo "Errors in System logs:"
+Get-EventLog -LogName System -EntryType Error
+echo ""
+echo "Errors in Application logs:"
+Get-EventLog -LogName Application -EntryType Error
+echo ""
+echo "Administrator account activity:"
+Get-EventLog -Log System -UserName "Administrator" | Group-Object -Property "UserName" -noelement | Format-Table Count, Name -Auto
+Get-EventLog -Log System -UserName "Administrator"
+$Aug26 = Get-Date 8/26/18
+$Sep26 = Get-Date 9/26/08
+Get-EventLog -Log "system" -EntryType Error -After $Aug26 -before $Sep26
